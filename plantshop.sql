460e93f8ba323c9aebc3bd4d4ce8bbe6a7fc8db3
Jakub <149338562+Kubulax@users.noreply.github.com>
2024-11-14 06:55:25 +0100

    Updated plantshop.sql
    Added admins table

--- plantshop.sql
+++ plantshop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 09, 2024 at 03:18 PM
--- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.2.12
+-- Czas generowania: 14 Lis 2024, 06:52
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `plantshop`
+-- Baza danych: `plantshop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(10) UNSIGNED NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `password_hash` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `login`, `password_hash`) VALUES
+(1, 'admin', '$2y$10$VGuHgbwIBv6Y2Lh9xdoyiO/ZqTuL.4hHe1KLyVw8Oz4oQ6wqym0Ky');
 
 -- --------------------------------------------------------
 
@@ -33,22 +52,20 @@ CREATE TABLE `plants` (
   `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
   `description` text NOT NULL,
   `treatment` text NOT NULL,
-  `image` text NOT NULL,
+  `image` text NOT NULL DEFAULT 'null.png',
   `price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `plants`
+-- Zrzut danych tabeli `plants`
 --
 
 INSERT INTO `plants` (`id`, `plant_categoires_id`, `name`, `description`, `treatment`, `image`, `price`) VALUES
-(1, 5, 'test_aloes', 'Świetna roślina, łatwa w pielęgnacji, idealna do ogrodu w pełnym słońcu!', 'Uwielbiam tę roślinę! Rośnie szybko i pięknie kwitnie wiosną.', 'flower1.jpg', 32.99),
-(2, 8, 'test_dracent', '', '', 'flower2.jpg', 25.50),
-(4, 2, '[value-3]', '[value-4]', '[value-5]', 'flower1.jpg', 0.00),
-(5, 2, '[value-3]', '[value-4]', '[value-5]', 'flower1.jpg', 0.00),
-(6, 2, '[value-3]', '[value-4]', '[value-5]', 'flower1.jpg', 0.00),
-(7, 2, '[value-3]', '[value-4]', '[value-5]', 'flower1.jpg', 0.00),
-(8, 2, '[value-3]', '[value-4]', '[value-5]', 'flower1.jpg', 0.00);
+(1, 1, 'test_aloe', 'Świetna roślina, łatwa w pielęgnacji, idealna do ogrodu w pełnym słońcu!', 'Uwielbiam tę roślinę! Rośnie szybko i pięknie kwitnie wiosną.', 'null.png', '32.99'),
+(2, 1, 'test_dracent', '', 'A', 'plant_673563d95d86a.jfif', '25.50'),
+(13, 1, 'test_aloe', 'A', 'A', 'plant_67356cd232657.jfif', '102.00'),
+(14, 2, 'test_aloes', 'a', 'a', 'plant_67357322e28df.jfif', '200.00'),
+(15, 3, 'test_dracent', 'AA', 'AA', 'plant_673573f3a4d0e.jpg', '5.00');
 
 -- --------------------------------------------------------
 
@@ -57,12 +74,12 @@ INSERT INTO `plants` (`id`, `plant_categoires_id`, `name`, `description`, `treat
 --
 
 CREATE TABLE `plant_categories` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
   `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `plant_categories`
+-- Zrzut danych tabeli `plant_categories`
 --
 
 INSERT INTO `plant_categories` (`id`, `name`) VALUES
@@ -82,6 +99,12 @@ INSERT INTO `plant_categories` (`id`, `name`) VALUES
 --
 
 --
+-- Indeksy dla tabeli `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
 -- Indeksy dla tabeli `plants`
 --
 ALTER TABLE `plants`
@@ -95,30 +118,26 @@ ALTER TABLE `plant_categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
--- AUTO_INCREMENT for table `plants`
+-- AUTO_INCREMENT dla tabeli `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `plants`
 --
 ALTER TABLE `plants`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT for table `plant_categories`
+-- AUTO_INCREMENT dla tabeli `plant_categories`
 --
 ALTER TABLE `plant_categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `plants`
---
-ALTER TABLE `plants`
-  ADD CONSTRAINT `plants_ibfk_1` FOREIGN KEY (`plant_categoires_id`) REFERENCES `plant_categories` (`id`);
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
